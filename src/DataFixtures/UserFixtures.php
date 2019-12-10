75b98610973ec01609cbd9b235588fba1fac31b8
Your Name <jesseonetoweb@gmail.com>
2019-12-10 13:27:55 +0100

    database

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -20,27 +20,13 @@ class UserFixtures extends Fixture
 public function load(ObjectManager $manager)
 {
     $user = new User();
-    // ...
 
-//             $user->setPassword($this->passwordEncoder-> encodePassword(
-//                     $user, 'test123'));
-//            $user->setEmail('jesse@gmail.com');
-//
-//    $user->setPassword($this->passwordEncoder-> encodePassword(
-//        $user, 'onetoshop'));
-//    $user->setEmail('gernand@gmail.com');
-    $user->setPassword($this->passwordEncoder-> encodePassword(
-        $user, 'admin'));
-    $user->setEmail('admin@gmail.com');
+    $user->setPassword($this->passwordEncoder-> encodePassword($user, 'admin'));
+    $user->setEmail('superadmin@gmail.com');
     $user->setUsername('Admin');
 
     $manager->persist($user);
 
-    // add more products
-
     $manager->flush();
-
-
-    // ...
 }
 }
